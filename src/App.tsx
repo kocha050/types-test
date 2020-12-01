2b33638a69cd403e85e55446fb7a28b015be69c6
kocha050 <kocha050@users.noreply.github.com>
2020-12-01 19:21:38 +0900

    4koma
    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useEffect, useState } from 'react'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <BrowserRouter>
+    <div className="container text-center mt-5">
+      <Route exact path="/" component={Login} />
+      <Route path="/home" component={Home} />
+      <Route path="/about" component={About} />
     </div>
-  );
+  </BrowserRouter>
+)
+
+const Login = () => {
+  return (
+    <div>
+      <h1>ログイン</h1>
+      <form>
+        ID：
+        <label>
+          <input type="text" name="name" />
+        </label>
+      </form>
+      <form>
+        PassWord：
+        <label>
+          <input type="text" name="name" />
+        </label>
+      </form>
+      <a className="btn login-button" href="/home">ログイン</a>
+    </div>
+  )
 }
 
-export default App;
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <h1>４コマリレー</h1>
+      </div>
+      <div>
+        <p>ここに画像</p>
+        <p><Link to="/about">Aboutへ</Link></p>
+      </div>
+    </div>
+
+  )
+}
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  )
+}
+
+const MyCounter = () => {
+  const [count, setCount] = useState<number>(0)
+  return (
+    <div>
+      <div>{count}</div>
+      <div>
+        <button onClick={() => setCount(count + 1)}>+1</button>
+        <button onClick={() => setCount(count - 1)}>-1</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
